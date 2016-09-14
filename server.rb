174c72308dcf833ce3ffeea2df864737f6c9f732
Oskari Virtanen <oskari@smartly.io>
2016-09-14 23:05:10 +0300

    Add redis url from env

--- server.rb
+++ server.rb
@@ -1,12 +1,14 @@
 require 'sinatra'
 require 'redis'
 
+REDIS_URL = ENV.fetch("REDISTOGO_URL", "redis://localhost:6379")
+
 get '/' do
   erb :index
 end
 
 post '/record' do
-  redis = Redis.new
+  redis = Redis.new(url: REDIS_URL)
   redis.incr('kaljaa')
 
   erb :success
