41245fae5f45cb9ab5dce74778f27d889a6c2e96
Oskari Virtanen <oskari@smartly.io>
2016-09-15 09:13:13 +0300

    Add customizable codes

--- server.rb
+++ server.rb
@@ -3,13 +3,27 @@ require 'redis'
 
 REDIS_URL = ENV.fetch("REDISTOGO_URL", "redis://localhost:6379")
 
-get '/salaurli' do
-  erb :index
+VALID_CODES = %w(salaurli smartly)
+
+get '/:code' do
+  if VALID_CODES.include?(params[:code])
+    @code = params[:code]
+    erb :index
+  else
+    "Wrong url :("
+  end
 end
 
 post '/record' do
   redis = Redis.new(url: REDIS_URL)
-  redis.incr('kaljaa')
+  redis.incr("kaljaa:#{params[:code]}")
 
+  @code = params[:code]
   erb :success
+end
+
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
 end
